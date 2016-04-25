5b56daf9ef8348686291a39da136249f3bee4894
Hannah Barnes <hlmbarnes@me.com>
2016-04-24 23:47:02 -0700

    updated and created a signup page that is inked from the home page with a href tags around sign up directing with EJS

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
 var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser')
 // var db = require('./models')
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/static'));
 
 
 app.get('/', function(req, res) {
-  res.send('Welcome to Hoppy Hour');
+  res.render('index');
+});
+
+app.get('/account/signup', function(req, res) {
+  res.render('signups');
 });
 
 var port = 3000;

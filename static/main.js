77b0932c3831c1e1972d497170e4af33a3d454c8
Hannah Barnes <hlmbarnes@me.com>
2016-04-27 17:16:35 -0700

    updated the longitude and latitude to be seattles for google maps

--- static/main.js
+++ static/main.js
@@ -6,7 +6,7 @@ var map = new google.maps.Map();
   function initMap() {
     var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
-      center: {lat: 44.540, lng: -78.546},
+      center: {lat: 47.6062, lng: -122.335167},
       zoom: 8
     });
   }

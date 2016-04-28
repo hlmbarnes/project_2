61f40f3fda0119eb51b3fdbfac4f88b6155e178d
Hannah Barnes <hlmbarnes@me.com>
2016-04-28 10:14:05 -0700

    updated the opacity and other styling of the map

--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 var map;
 var infowindow;
-
+var service;
 
 function initialize() {
   var Seattle = new google.maps.LatLng(47.6062, -122.335167);
@@ -15,40 +15,47 @@ function initialize() {
   var request = {
     location: Seattle,
     radius: '500',
-    types: ['bar', 'brewery', 'beer']
+    keyword: 'brewery'
   };
    
   infowindow = new google.maps.InfoWindow();
 
   // Create the PlaceService and send the request.
   // Handle the callback with an anonymous function.
-  var service = new google.maps.places.PlacesService(map);
+  service = new google.maps.places.PlacesService(map);
 
-  service.nearbySearch(request, callback);
+  service.radarSearch(request, callback);
 }
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-           createMarker(results[i]);
 
-      }
-    }
+        for (var i = 0; i < results.length; i++) {
+        var request = {placeId: results[i].place_Id};
+             createMarker(results[i]);     
+        }
+ 
   }
+  else{ alert(status)};
+}
 
-function createMarker(place){
-  alert (place.name + " detail: "+ place.formatted_address);
-  var placeLoc = place.geometry.location;
+function createMarker(placeOK){
   var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
+        map: map,
+        position: placeOK.geometry.location
+      });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent('<div><strong>' + place.name + '</strong><br></div>');
-    infowindow.open(map, this);
+      google.maps.event.addListener(marker, 'click', function() {
+        service.getDetails({placeId: placeOK.place_id}, function(place, status){
+        if (status === google.maps.places.PlacesServiceStatus.OK){
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+              place.formatted_address + '</div>');
+              infowindow.open(map, marker);
+            }
+            else{ alert(status)};
+      });
+    
   });
-
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
